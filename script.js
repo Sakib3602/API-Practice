c9093768f5cb19d315f409f14ada2feee11b37d3
Sakib3602 <sakibsarker6969@gmail.com>
2024-02-28 17:19:46 +0600

    added modal

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 let api = async () => {
   let res = await fetch("https://openapi.programming-hero.com/api/ai/tools");
   let data = await res.json();
-  // console.log(data.data.tools)
   let tool = data.data.tools;
-
-  // let x = tool.map(e => console.log(e.name))
+//   console.log(data)
 
   set(tool);
 
-  // console.log(tool[0].features[0])
+  modal(tool);
 };
 api();
 
@@ -16,14 +14,12 @@ function set(s) {
   let card = document.getElementById("card");
 
   s.forEach((s1) => {
-    console.log(s1);
-
     let cre = document.createElement("div");
     cre.classList = "card w-96  bg-base-100 shadow-xl p-5";
 
     cre.innerHTML = `
 
-    <figure><img class="h-[250px]" src="${s1.image}" alt="Shoes" /></figure>
+    <figure><img onclick="modal('${s1.id}'); my_modal_3.showModal()"  class="h-[250px]" src="${s1.image}" alt="Shoes" /></figure>
             <div class="card-body">
               <h2 class="card-title mb-[10px]">
                 Features
@@ -53,9 +49,41 @@ function set(s) {
              
               <h2>${s1.name}</h2>
               <h4>${s1.published_in}</h4>
+
             </div>
     
     `;
     card.appendChild(cre);
   });
+}
+
+let getId = async (get) => {
+  
+
+  let res = await fetch(
+    `https://openapi.programming-hero.com/api/ai/tool/${get}`
+  );
+  let data = await res.json();
+
+  
+made(data.data)
+  
+};
+
+function made(p){
+
+console.log("p",p)
+    let name = document.getElementById("name")
+    name.innerText = p.tool_name
+    // name.innerText = `${p.tool_name}`
+    
+
+}
+
+function modal(op) {
+ 
+
+  
+
+  getId(op);
 }
